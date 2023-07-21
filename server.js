b1a470416216715f29a16e71e34d72e90005b52a
ADOFO-09 <emmanueladofo011@gmail.com>
2023-07-21 05:30:44 +0100

    Making a post request to check number of entries

--- server.js
+++ server.js
@@ -67,9 +67,26 @@ app.get('/profile/:id', (req, res)=> {
     }
 })
 
+// Making a post request for number of entries
+app.post('/image',(req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(404).json('not found');
+    }
+})
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 })
+
 
 
 /*
