4f2f2d1fa7e1ec7885e64b6df8f71a1bccc9488b
ADOFO-09 <emmanueladofo011@gmail.com>
2023-07-26 05:57:37 +0100

    minor change

--- server.js
+++ server.js
@@ -12,14 +12,16 @@ const database = {
     {
         id: '123',
         name: 'John',
-        email: 'john@example.com',
+        email: 'john@gmail.com',
+        password: 'cookies',
         entries: 0,
         joined: new Date()
     },
     {
         id: '124',
         name: 'Sally',
-        email: 'Sally@example.com',
+        email: 'Sally@gmail.com',
+        password: 'apples',
         entries: 0,
         joined: new Date()
     }
