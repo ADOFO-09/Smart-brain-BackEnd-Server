57a8d964c26db9db39dc57f7bb731da2d9a4fad5
ADOFO-09 <emmanueladofo011@gmail.com>
2023-08-04 04:40:12 +0100

    Change app.image to put request

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/profile/:id', (req, res)=> {
 })
 
 // Making a post request for number of entries
-app.post('/image',(req, res) => {
+app.put('/image',(req, res) => {
     const {id} = req.body;
     let found = false;
     database.users.forEach(user => {
