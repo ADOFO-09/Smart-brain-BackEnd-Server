9eb8ac90b3ad96b42d59d7abf38e16d17cf5424e
ADOFO-09 <emmanueladofo011@gmail.com>
2023-09-03 21:55:42 +0100

    getting user profiles

--- server.js
+++ server.js
@@ -91,16 +91,17 @@ app.post('/register', (req, res) => {
 // Making a get request using user id
 app.get('/profile/:id', (req, res)=> {
     const {id} = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({id})
+        .then(user => {
+        if(user.length){
+            res.json(user[0])
+        } else {
+            res.status(400).json("Not found")
         }
+        
     })
-    if (!found) {
-        res.status(404).json('not found');
-    }
+      .catch(err => res.status(400).json("error getting user"))
+    
 })
 
 // Making a put request for number of entries
