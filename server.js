ab6e1789a3d23df1cd4d2de460a19f61f3458ecd
ADOFO-09 <emmanueladofo011@gmail.com>
2023-09-06 05:45:56 +0100

    transacting user details to login

--- server.js
+++ server.js
@@ -84,18 +84,18 @@ app.post('/register', (req, res) => {
             return trx('users')
                 .returning('*')
                 .insert({
-                    email: loginEmail[0],
+                    email: email,
                     name: name,
                     joined: new Date()
                 })
                 .then(user => {
-                    res.json(user[0])
+                    res.json(user[0]);
                 })
-            })  
-            .then(trx.commit)
-            .catch(trx.rollback)
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json("unable to resgister"))   
+    .catch(err => res.status(400).json("unable to register"));
 })
 
 // Making a get request using user id
