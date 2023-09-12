56c3a35b9e73dc7af2c828e3a7dcbdf3cac15912
ADOFO-09 <emmanueladofo011@gmail.com>
2023-09-12 15:07:11 +0100

    Code Review

---
+++ controllers/image.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Body parsing middleware
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id','=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+                res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json("unable to get count"));
+}
+
+module.exports = {
+    handleImage
+}
---
+++ controllers/profile.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Body parsing middleware
+
+const handleProfileGet = (req,res, db)=> {
+    const {id} = req.params;
+    db.select('*').from('users').where({id})
+        .then(user => {
+        if(user.length){
+            res.json(user[0])
+        } else {
+            res.status(400).json("Not found")
+        }
+        
+    })
+    .catch(err => res.status(400).json("error getting user"))
+    
+}
+
+module.exports = {
+    handleProfileGet
+}
